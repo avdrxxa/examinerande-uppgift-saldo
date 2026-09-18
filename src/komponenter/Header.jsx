d874dd9f54dc6cc21e908fe86ab419c54de6618a
avdrxxa <damian.raluka.andreea23@gmail.com>
2026-09-18 19:44:32 +0200

    Not much just h2 css and done

--- src/komponenter/Header.jsx
+++ src/komponenter/Header.jsx
@@ -3,7 +3,6 @@ import logoText from '../assets/logo text.svg'
 import Betala from './Betala'
 import Inkomst from './Inkomst'
 import BytValuta from './BytValuta'
-import { Link } from 'react-router-dom'
 
 export default function Header({isHome}){
     return(
@@ -13,7 +12,6 @@ export default function Header({isHome}){
             <img src={logoText}></img>
         </div>
             {isHome && <div className='actions'> <Betala/> <Inkomst/> <BytValuta/> </div>}
-            {!isHome && <p className='linkBack'><Link to={'/'}>{`< Back Home `}</Link></p>}
         </nav>
         )
 }
--- src/pages/Transaktioner.jsx
+++ src/pages/Transaktioner.jsx
@@ -1,11 +1,14 @@
 
 import Header from "../komponenter/Header"
 import AllaTransaktioner from "../komponenter/AllaTransaktioner"
+import { Link } from "react-router-dom"
+
 export default function Transaktioner(){
     return(
         <>
             <Header isHome={false}/>
-            <main>
+            <main className="mainTrans">
+                <Link to='/'>Back Home</Link>
                 <h2>Senaste transaktioner</h2>
                 <AllaTransaktioner/>
             </main>
